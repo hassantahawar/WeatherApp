995f9f9d4bd7b477f87ab9a4333546976a51b716
Hassan <hassantahawar4u@gmail.com>
2021-02-07 19:52:47 +0500

    heroku changes

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require("./utils/geocode");
 const openWeatherMap = require("./utils/openWeatherMap");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //paths of directory
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -73,6 +74,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000");
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
 });
